a550952454c459d36471e61cde7a9fc2df406c63
Gabriel Maguna <gabimaguna@gmail.com>
2022-09-17 18:26:28 -0300

    config database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import app from './app.js'
-import https from 'https'
-import fs from 'fs'
+import app from "./app.js";
+import https from "https";
+import * as fs from "fs";
+import { log } from "console";
 
-https.createServer({
-    key: fs.readFileSync('../certs/key.pem'),
-    cert: fs.readFileSync('../certs/cert.pem')
-}, app).listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
-})
+https
+  .createServer(
+    {
+      key: fs.readFile("./src/certs/key.pem", (error, data) => {
+        if (error) {
+          console.log(error);
+        }
+        console.log("key aprobada!");
+        return data;
+      }),
+      cert: fs.readFile("./src/certs/certificate.pem", (error, data) => {
+        if (error) {
+          console.log(error);
+        }
+        console.log("certificado aprobado!")
+        return data;
+      }),
+    },
+    app
+  )
+  .listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
